39f13f0bdc9b63d18d09565aea11840c4204eedb
Cy Villaseñor <cyvillasenor@Cys-BacMook-Pro.lan>
2025-07-14 09:01:26 +0800

    update

--- addEODFile.py
+++ addEODFile.py
@@ -18,7 +18,7 @@ def generate_eod_filenames(start_date=None):
 
     return filenames
 
-folder_path = "EOD/06June/Week 5/"
+folder_path = "EOD/07July/Week 1/"
 
 os.makedirs(folder_path, exist_ok=True)
 
